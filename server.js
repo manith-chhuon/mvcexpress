342e4c6feb55c5009717a135bccc331ae8ea27fb
Manith <manith.chhuon@niptict.edu.kh>
2024-12-24 14:27:12 +0700

    add image as display in index

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const productRoutes = require('./routes/productRoutes');
+const session = require('express-session');
 
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
 // Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 app.set('view engine','ejs');
 
 const PORT = process.env.PORT || 3000;
 
+app.use('/uploads', express.static('public/uploads'));
 app.use('/product',productRoutes);
 
 app.listen(PORT,()=>{
